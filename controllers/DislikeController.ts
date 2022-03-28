827b74c47f262cbc6c429ffdb71ace45269964f1
vivek <vivek>
2022-03-28 00:47:57 -0400

    updated server.ts to add dislikes controller

--- controllers/DislikeController.ts
+++ controllers/DislikeController.ts
@@ -36,9 +36,9 @@ export default class DislikeController implements DislikeControllerI {
     public static getInstance = (app: Express): DislikeController => {
         if(DislikeController.dislikeController === null) {
             DislikeController.dislikeController = new DislikeController();
-            app.get("/api/users/:uid/likes", DislikeController.dislikeController.findAllTuitsDislikedByUser);
-            app.get("/api/tuits/:tid/likes", DislikeController.dislikeController.findAllUsersThatDislikedTuit);
-            app.put("/api/users/:uid/likes/:tid", DislikeController.dislikeController.userTogglesTuitDislikes);
+            app.get("/api/users/:uid/dislikes", DislikeController.dislikeController.findAllTuitsDislikedByUser);
+            app.get("/api/tuits/:tid/dislikes", DislikeController.dislikeController.findAllUsersThatDislikedTuit);
+            app.put("/api/users/:uid/dislikes/:tid", DislikeController.dislikeController.userTogglesTuitDislikes);
         }
         return DislikeController.dislikeController;
     }
